0c8c89265930d1aea7752c0a1caa64f40ec7934d
Carlos Félix <carlos.lima.felix@gmail.com>
2021-01-27 17:12:23 -0300

    Finalização da estrutura de componentes do formulário

--- react/src/App.js
+++ react/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      
+
+      <section className="Search-area">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="logo" /> 
       </header>
+        <div className="Search">
+          <select name="País" id="Pais" value="Selecione...">
+              <option></option>
+          </select>
+          <input type="search" name="Local" id="Local" value="Digite o local que deseja conhecer"/>
+          <input type="month" name="Meta" id="Meta" value="mês/ano"/>
+          <input type="submit" name="Adicionar" id="Input"/>
+
+        </div>
+      </section>
     </div>
   );
 }
